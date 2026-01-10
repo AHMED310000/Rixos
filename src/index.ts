a2d9277a4f5e76aba944853ed855ca8c3bdcc548
AHMED310000 <ahmedsalim3100.as@gmail.com>
2026-01-10 14:07:52 +0200

    Change server port from 4000 to 5000

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import express from "express";
 const app = express();
 app.use(express.json());
 app.get("/", (req, res) => {res.send("Rixos backend is running");});
-app.listen(4000, "0.0.0.0",() => {console.log("Server running on port 4000");});
+app.listen(5000, "0.0.0.0",() => {console.log("Server running on port 5000");});
