875e2352c3f3a3139d9621ed4ca7e39abeec29e9
Ahmed <ahmed310000@gmail.compwdgit config --global user.email ahmed310000@gmail.com>
2026-01-11 19:31:13 +0200

    Initial commit with rixos files

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,11 @@
 import express from "express";
-
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const HOST = "0.0.0.0";
-
 app.get("/", (req, res) => {
-  res.send("Rixos backend is running");
+    res.send("Rixos backend is running");
 });
-
 app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+    console.log(`Server running on http://${HOST}:${PORT}`);
 });
-
