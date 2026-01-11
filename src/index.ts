06f8a094fa533b90058534abced0983c5d99b6b4
Ahmed Salim <ahmedsalim3100.as@gmail.com>
2026-01-11 11:54:59 +0200

    Merge local changes with remote changesMerge local changes with remote changes
    Upload project from Termux

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import express from "express";
+
 const app = express();
 app.use(express.json());
+ HEAD
 app.get("/", (req, res) => {res.send("Rixos backend is running");});
 app.listen(5000, "0.0.0.0",() => {console.log("Server running on port 5000");});
+
+const PORT = 5000;
+const HOST = "0.0.0.0";
+
+app.get("/", (req, res) => {
+  res.send("Rixos backend is running");
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
+});
+>>>>>>> 71d8721 (Upload project from Termux)
