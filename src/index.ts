52e49066fd09a927cd2eec3fb62f57c5124d66c2
AHMED310000 <ahmedsalim3100.as@gmail.com>
2026-01-11 19:55:17 +0200

    Fix formatting and add newline at end of file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import express from "express";
-
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 const HOST = "0.0.0.0";
-
 app.get("/", (req, res) => {
   res.send("Rixos backend is running");
 });
-
 app.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
 });
-
